fea4d5c9bf75c263bf4b478f37f689ac2109c237
catsmile <catsmile.nico@gmail.com>
2020-08-18 00:59:13 +0900

    unity first commit - 2D character controller (idle, walk, run, jump)

---
+++ 2D Tutorial/Assets/Scripts/Fox.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    // Public
+    public float speed;
+
+    // Private
+    Rigidbody2D rb;
+    Animator animator;
+    [SerializeField] Transform groundCheckCollider;
+    [SerializeField] LayerMask groundLayer;
+
+    const float groundCheckRadius = 0.2f;
+    float horizontalValue;
+    float runSpeedModifier = 2f;
+    [SerializeField] float jumpPower = 100;
+    bool jump = false;
+    bool facingRight = true;
+    [SerializeField] bool isRunning = false;
+    [SerializeField] bool isGrounded = false;
+    
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+        animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        GroundCheck();
+        Move(horizontalValue, jump);
+    }
+
+    void GroundCheck()
+    {
+        isGrounded = false;
+        //check if groundcheckobject colliding with other
+        //2d colliders in "Ground" layer
+        //if yes (isGround true) else (isGrounded false)
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
+        if (colliders.Length > 0)
+            isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //store horizontal value
+        horizontalValue = Input.GetAxisRaw("Horizontal") ;
+
+        //if LShift pressed, enable isRunning
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            isRunning = true;
+        //if LShift released, disable isRunning
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            isRunning = false;
+
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+        else if (Input.GetButtonUp("Jump"))
+            jump = false;
+    }   
+
+    void Move(float direction, bool jumpFlag)
+    {
+        //if player is grounded and space pressed Jump
+        if(isGrounded && jumpFlag)
+        {
+            isGrounded = false;
+            jumpFlag = false;
+            rb.AddForce(new Vector2(0f,jumpPower));
+        }
+
+        #region Move & Run
+        //set val of x using dir and speed
+        float xVal = direction * speed * 100 * Time.fixedDeltaTime;
+        //if running multiply with modifier
+        if (isRunning)
+            xVal *= runSpeedModifier;
+        //cre vec2 for velocity
+        Vector2 targetVelocity = new Vector2(xVal, rb.velocity.y);
+        //set player velocity
+        rb.velocity = targetVelocity;
+
+        //if looking right and clicked left (flip to left)
+        if(facingRight && direction<0)
+        {
+            transform.localScale = new Vector3(-4,4,4);
+            facingRight = false;
+        }
+        //if looking left and clicked right (flip to right)
+        else if(!facingRight && direction>0)
+        {
+            transform.localScale = new Vector3(4,4,4);
+            facingRight = true;
+        }
+
+        //idle 0, walking 10, running 20
+        //Set float xVelocity according to the x value of the rigidbody velocity
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+        #endregion
+    }
+}
